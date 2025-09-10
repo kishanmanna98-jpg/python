1373f15580be5496dde5f43540e5dfbba3364bc3
Kishan manna <kishanmanna98@gmail.com>
2025-09-10 18:16:14 +0530

    Add files via upload

---
+++ def.py
@@ -0,0 +1,13 @@
+def big():
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    c=int(input("enter a number"))
+    if(a>b and a>c):
+        print(a)
+    elif(b>a and b>c):
+        print(b)
+    else:
+        print(c)
+big()
+    
+            
