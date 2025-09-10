753bfdf7bc6ca118d8a3562ecfaaabf119eaa693
Kishan manna <kishanmanna98@gmail.com>
2025-09-10 17:46:18 +0530

    Add files via upload

---
+++ ak487.py
@@ -0,0 +1,6 @@
+d={"eno":123,"ename":"xyz"}
+print(d)
+print(d["eno"])
+print(d["ename"])
+for key,values in d.items():
+    print(key,":",values)
---
+++ iiik.py
@@ -0,0 +1,9 @@
+l=[1,2,3,4]
+sum=0
+for i in l:
+   sum=sum+i
+avg=sum/4
+print(sum)
+print(avg)
+
+
