f46f9b97c596fec130576e42947d8b1349a07925
Kishan manna <kishanmanna98@gmail.com>
2025-09-19 17:00:57 +0530

    Add files via upload

--- class.file.py
+++ class.file.py
@@ -4,14 +4,23 @@ class marks:
         self.mark2=m2
         self.mark3=m3
     def calculate(self):
-        tot=m1+m2+m3
-        avg=tot/3
-        print('total marks',tot)
-        print('average',avg)
+        self.tot=self.mark1+self.mark2+self.mark3
+        self.avg=self.tot/3
+        print('total marks',self.tot)
+        print('average',self.avg)
+    def display(self):      
+        print("m1:",self.mark1)
+        print("m2:",self.mark2)
+        print("m3:",self.mark3)
 m1=int(input("enter m1:"))
 m2=int(input("enter m2:"))
-m3=int(input("enter m3:"))      
+m3=int(input("enter m3:"))
+if m1>40 and m2>40 and m3>40:
+    print("pass")
+else:
+    print("fail")
 od=marks(m1,m2,m3)
+od.display()
 od.calculate()
 
         
